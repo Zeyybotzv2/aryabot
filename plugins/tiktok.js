72997593a1f9f3fa960083cbda0c68cefe315eef
Zeyybotzv2 <109266556+Zeyybotzv2@users.noreply.github.com>
2022-07-14 14:50:43 +0800

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -22,7 +22,7 @@ if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\n
 if (!args[0].match(/tiktok/gi)) throw `*Link salah! Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
 let p = await  hxz.ttdownloader(args[0])
 const { nowm, wm, audio } = p
-// made by arietube
+// made by ZeyyBotz
  conn.sendFile(m.chat, nowm, 'tiktok.mp4', `*${global.wm}*`, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
